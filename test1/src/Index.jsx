a618aaf28b438f2dd81ca4d1825fa5fa0210d556
akprinciple <akeemolayiwola9@gmail.com>
2025-05-17 11:39:37 +0100

    Next one

--- test1/src/Index.jsx
+++ test1/src/Index.jsx
@@ -1,25 +1,33 @@
-import React from 'react'
-import Header from './Header'
-const ProductList = () => {
-  const [products, setProducts] = useState([]);
+import React, { useState, useEffect } from 'react';
+import Header from './Header';
+
+
+const Index = () => {
+    const [products, setProducts] = useState([]);
 
   useEffect(() => {
     fetch("https://fakestoreapi.com/products")
       .then(res => res.json())
       .then(data => setProducts(data));
   }, []);
-}
-const Index = (prop) => {
+
   return (
-   <>
-    <Header />
-    <h1>All Products</h1>
-     {products.map(product => (
-        <ProductCard key={product.id} product={product} />
+    <>
+      {/* <Header /> */}
+      <h1>All Products</h1>
+         <div className="product-grid">
+      {products.map(product => (
+        <div className="product-card" key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <h2>{product.title}</h2>
+          <p>{product.description}</p>
+          <p>Price: ${product.price}</p>
+          <button>Add to Cart</button>
+        </div>
       ))}
-   </>
-     )
+    </div>
+    </>
+  )
 }
 
 export default Index
-
